11575d916f4b062daaaf7bac04b76d6f6018bb29
markosanchez800 <markosanchez800@gmail.com>
2021-05-16 17:05:09 -0700

    seeds are appearing on site(pics not rendering)

--- picclick/src/App.js
+++ picclick/src/App.js
@@ -1,8 +1,25 @@
-import React from "react";
-import PortfolioContainer from "./components/PortfolioContainer";
+import React, {Component} from "react";
+import GameCard from './components/GameCards';
+import vaultboy from './vaultboy.json';
 
-function App() {
-  return <PortfolioContainer />;
+class App extends Component {
+  state = {
+    vaultboy: vaultboy
+  };
+
+
+render() {
+  return (
+    <div>
+  {this.state.vaultboy.map(vaultboy => (
+    <GameCard
+    id={vaultboy.id}
+    name={vaultboy.name}
+    image={vaultboy.image}
+    />
+  ))}
+  </div>
+  );
 }
-
+}
 export default App;
--- picclick/src/components/gameCards/index.js
+++ picclick/src/components/gameCards/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './style.css';
 
-function gameCard(props){
+
+function GameCard(props){
     return (
         <div className='card'>
             <div className='img-container'>
-                <img alt={props.name} src={props.image}/> 
+                <img alt={props.name} src={props.image} /> 
             </div>
         </div>
     );
 }
 
-export default gameCard;
+export default GameCard;
